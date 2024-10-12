fb2780abae973a27e46e58feb8240550adfa4431
pimPijnenburg <pimmohs@gmail.com>
2024-10-12 22:03:59 +0200

    Update
    Creation of benchmark, some changes to the transfer learning script and (hopefully) more efficient code

--- USTC_SmokeRS_dataset/code/transfer_learning/tl_tools.py
+++ USTC_SmokeRS_dataset/code/transfer_learning/tl_tools.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np 
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers, models, mixed_precision
 
 def prepare_transferlearn(transfer_learning_architecture, name = 'Model'): 
     n_classes = 6
@@ -9,14 +9,51 @@ def prepare_transferlearn(transfer_learning_architecture, name = 'Model'):
     model.add(transfer_learning_architecture)
     model.add(layers.GlobalAveragePooling2D())
     model.add(layers.Dense(128, activation= 'relu'))
-    model.add(layers.Dense(n_classes, activation= 'softmax'))
+    model.add(layers.Dense(n_classes, activation= 'softmax', dtype = 'float32'))
     return model
 
 
+def setup_mixed_precision():
+    policy = mixed_precision.Policy('mixed_float16')
+    mixed_precision.set_global_policy(policy)
+    print("Mixed precision policy set to:", policy.name)
 
-def load_data(path = '/Users/pimpijnenburg/Desktop/Thesis/USTC_SmokeRS_dataset/data/created_data/for_model_training', batch_size = 64): 
-    train = tf.data.Dataset.load(os.path.join(path, 'train')).batch(batch_size= batch_size)
-    val = tf.data.Dataset.load(os.path.join(path, 'val')).batch(batch_size= batch_size)
-    test = tf.data.Dataset.load(os.path.join(path, 'test')).batch(batch_size= batch_size)
+def create_optimizer(learning_rate=1e-3):
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    return mixed_precision.LossScaleOptimizer(optimizer) 
+    
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+def optimize_dataset(dataset, batch_size, is_training=False):
+    dataset = dataset.batch(batch_size)
+    
+    if is_training:
+        dataset = dataset.shuffle(buffer_size=1000)
+    
+    dataset = dataset.prefetch(buffer_size=AUTOTUNE)
+    
+    return dataset
+
+def load_data(path='/Users/pimpijnenburg/Desktop/Thesis/USTC_SmokeRS_dataset/data/created_data/for_model_training', batch_size=64):
+    train = tf.data.Dataset.load(os.path.join(path, 'train'))
+    val = tf.data.Dataset.load(os.path.join(path, 'val'))
+    test = tf.data.Dataset.load(os.path.join(path, 'test'))
+    
+    train = optimize_dataset(train, batch_size, is_training=True)
+    val = optimize_dataset(val, batch_size)
+    test = optimize_dataset(test, batch_size)
     
     return train, val, test
+
+
+def create_baseline(architecture, name = 'EfficentNetV2'): 
+    n_classes = 6
+    output = architecture.output
+    output = layers.GlobalAveragePooling2D()(output)
+    output = layers.Dense(1024, activation = 'relu')(output)
+    output = layers.Dropout(0.5)(output)
+    outputs = layers.Dense(n_classes, activation = 'softmax', dtype = 'float32')(output)
+    
+    model = tf.keras.Model(inputs = architecture.input, outputs = outputs)
+    return model
