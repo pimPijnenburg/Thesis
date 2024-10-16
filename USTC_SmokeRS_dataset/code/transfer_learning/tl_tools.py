3d0e9de4e104badd4f7b36090802a24e022e0c20
pimPijnenburg <pimmohs@gmail.com>
2024-10-16 10:24:42 +0200

    How tf do i overcome a dying kernel lol

--- USTC_SmokeRS_dataset/code/transfer_learning/tl_tools.py
+++ USTC_SmokeRS_dataset/code/transfer_learning/tl_tools.py
@@ -2,15 +2,7 @@ import os
 import numpy as np 
 import tensorflow as tf
 from tensorflow.keras import layers, models, mixed_precision
-
-def prepare_transferlearn(transfer_learning_architecture, name = 'Model'): 
-    n_classes = 6
-    model = models.Sequential(name = name)
-    model.add(transfer_learning_architecture)
-    model.add(layers.GlobalAveragePooling2D())
-    model.add(layers.Dense(128, activation= 'relu'))
-    model.add(layers.Dense(n_classes, activation= 'softmax', dtype = 'float32'))
-    return model
+import pandas as pd
 
 
 def setup_mixed_precision():
@@ -35,25 +27,47 @@ def optimize_dataset(dataset, batch_size, is_training=False):
     
     return dataset
 
-def load_data(path='/Users/pimpijnenburg/Desktop/Thesis/USTC_SmokeRS_dataset/data/created_data/for_model_training', batch_size=64):
-    train = tf.data.Dataset.load(os.path.join(path, 'train'))
-    val = tf.data.Dataset.load(os.path.join(path, 'val'))
-    test = tf.data.Dataset.load(os.path.join(path, 'test'))
-    
-    train = optimize_dataset(train, batch_size, is_training=True)
-    val = optimize_dataset(val, batch_size)
-    test = optimize_dataset(test, batch_size)
-    
-    return train, val, test
 
 
-def create_baseline(architecture, name = 'EfficentNetV2'): 
-    n_classes = 6
-    output = architecture.output
-    output = layers.GlobalAveragePooling2D()(output)
-    output = layers.Dense(1024, activation = 'relu')(output)
-    output = layers.Dropout(0.5)(output)
-    outputs = layers.Dense(n_classes, activation = 'softmax', dtype = 'float32')(output)
-    
-    model = tf.keras.Model(inputs = architecture.input, outputs = outputs)
-    return model
+
+def load_and_prepare_data(path, batch_size=32):
+    # Load the dataframes
+    train_df = pd.read_parquet(os.path.join(path, 'train.parquet'))
+    val_df = pd.read_parquet(os.path.join(path, 'val.parquet'))
+    test_df = pd.read_parquet(os.path.join(path, 'test.parquet'))
+
+    def preprocess_image(red, green, blue, label):
+        image = tf.stack([red, green, blue], axis=-1)
+        image = tf.reshape(image, (256, 256, 3))
+        image = tf.cast(image, tf.float32) / 255.0
+        label = tf.one_hot(label, depth=6)
+        return image, label
+
+    def create_dataset(df, shuffle=True):
+        dataset = tf.data.Dataset.from_tensor_slices((
+            df['red_channel'].tolist(),
+            df['green_channel'].tolist(),
+            df['blue_channel'].tolist(),
+            df['class'].tolist()
+        ))
+        
+        dataset = dataset.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+        
+        if shuffle:
+            dataset = dataset.shuffle(buffer_size=len(df))
+        
+        dataset = dataset.batch(batch_size)
+        dataset = dataset.prefetch(tf.data.AUTOTUNE)
+        
+        return dataset
+
+    train_dataset = create_dataset(train_df)
+    val_dataset = create_dataset(val_df, shuffle=False)
+    test_dataset = create_dataset(test_df, shuffle=False)
+
+    return train_dataset, val_dataset, test_dataset
+
+
+
+
+
